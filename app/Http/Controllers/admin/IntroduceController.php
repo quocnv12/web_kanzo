1d24944515c640c1ba7321196e9568c8068b2f0c
Pham Phuong <it.pdphuong@gmail.com>
2019-11-15 10:00:40 +0700

    phg

--- app/Http/Controllers/admin/IntroduceController.php
+++ app/Http/Controllers/admin/IntroduceController.php
@@ -14,4 +14,30 @@ class IntroduceController extends Controller
     	$introduce = introduce::all();
     	return view('admins.introduce.list',compact('introduce'));
     }
+    public function add(){
+    	return view('admins.introduce.add');
+    }
+    public function post_add(Request $req,$id){
+    	$this ->validate($req,[
+    		'title' => 'required|unique:introduce,title',
+    		'content' => 'required',
+    	],[
+    		'title.required' => 'Tiêu đề là trường bắt buộc!'
+    		'title.unique' => 'Tiêu đề đã tồn tại!'
+    		'content.required' => 'Nội dung là trường bắt buộc!'
+    	]);
+    	$new = new introduce;
+    	$new->title = $req->title;
+    	$new->slug = str_slug($req->title);
+    	$new->content = $req->content;
+    	$new->type = $req->1;  //1:giới thiệu - 2:chính sách
+    	$new->active = $req->active;
+    	$new->save();
+
+    	return redirect()->route('introduce.list');
+    }
+    public function edit($id){
+    	$introduce = introduce::find($id);
+    	return view('admins.introduce.edit',compact('introduce'));
+    }
 }
