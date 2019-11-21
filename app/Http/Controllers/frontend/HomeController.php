573e40280aef579d87d693199b5ef463707849dc
Pham Phuong <it.pdphuong@gmail.com>
2019-11-21 14:27:40 +0700

    phgupp

--- app/Http/Controllers/frontend/HomeController.php
+++ app/Http/Controllers/frontend/HomeController.php
@@ -15,8 +15,11 @@ class HomeController extends Controller
 		$data['popup'] = popup::first();
 		$data['question'] = question::orderBy('updated_at','DESC')->limit(2)->get();
 		$data['product'] = product::orderBy('id','DESC')->limit(5)->get();
-		$data['product1'] = product::find(1);
-		$data['product1'] = product::find(2);
+		$data['product1'] = product::find(6);
+		$data['product2'] = product::find(4);
+		$data['product3'] = product::find(5);
+		$data['product4'] = product::find(10);
+		$data['product5'] = product::find(11);
 		$data['intro_general'] = intro_general::first();
 		return view('pages.home',$data);
 	}
